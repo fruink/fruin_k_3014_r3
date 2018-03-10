6c970f863afc2e1af3d6bb4becfced81ffd75a72
fruink <31993241+fruink@users.noreply.github.com>
2018-03-10 00:20:00 -0500

    Delete init.php

--- fruin_karlee_3014_r3/admin/phpscripts/init.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require_once('functions.php');
-	require_once('read.php');
-	require_once('login.php');
-	require_once('sessions.php');
-	require_once('user.php');
-	require_once('userMail.php');
-	//require_once('forgotpass.php');
-?>
